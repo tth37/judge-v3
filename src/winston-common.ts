2d984d1dd2d49736d490b15d9bb4030e79cf148c
seucpc <admin@seucpc.club>
2021-06-03 20:05:58 +0800

    remove unused code

--- src/winston-common.ts
+++ src/winston-common.ts
@@ -1,11 +1,4 @@
 import winston = require('winston');
-import _ = require('lodash');
-import util = require('util');
-
-function formatter(args) {
-    var msg = args.level + ' - ' + args.message + (_.isEmpty(args.meta) ? '' : (' - ' + util.inspect(args.meta)));
-    return msg;
-}
 
 export function configureWinston(verbose: boolean) {
     winston.configure({
